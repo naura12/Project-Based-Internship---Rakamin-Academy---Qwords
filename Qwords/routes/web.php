74a4c92040ca9852bc20221f5176c8b1160eb819
Naura Sheika Maryam <91835294+naura12@users.noreply.github.com>
2023-12-04 22:24:28 +0700

    Add files via upload

---
+++ Qwords/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/Qwords', function () {
+    return view('Qwords');
+});
---
+++ Qwords/storage/framework/views/db717a208ec5e157fe0f33dda53960f5.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="icon" href="asset/qwords logo.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css"/>
+    <title>Qwords</title>
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="Qwords">
+                <img src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/logo-qw-light.webp" alt="qwords" width="89.63px" height="35px">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse py-3 py-xl-0 px-3" id="navbarNav">
+                <ul class="navbar-nav mx-auto gap-3">
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold">
+                            Cloud Hosting
+                            <i class="fa-solid fa-angle-down" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Cloud Hosting Lite</a></li>
+                            <li><a class="dropdown-item" href="#">Cloud Hosting Pro</a></li>
+                            <li><a class="dropdown-item" href="#">Cloud Hosting Enterprise</a></li>
+                            <li><a class="dropdown-item" href="#">Cloud Hosting WP</a></li>
+                            <li><a class="dropdown-item" href="#">Unlimited Hosting</a></li>
+                            <li><a class="dropdown-item" href="#">Private label cPanel Reseller</a></li>
+                            <li><a class="dropdown-item" href="#">Flex WP</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold">
+                            Server
+                            <i class="fa-solid fa-angle-down" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Cloud VPS KVM SSD</a></li>
+                            <li><a class="dropdown-item" href="#">Server Sekolah CBT/Moodle</a></li>
+                            <li><a class="dropdown-item" href="#">Dedicated Server</a></li>
+                            <li><a class="dropdown-item" href="#">Colocation Server</a></li>
+                            <li><a class="dropdown-item" href="#">Leased To Owned Server</a></li>
+                            <li><a class="dropdown-item" href="#">Fleksibel VPS</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold">
+                            Domain
+                            <i class="fa-solid fa-angle-down" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Daftar/Transfer Domain</a></li>
+                            <li><a class="dropdown-item" href="#">Domain Premium .id</a></li>
+                            <li><a class="dropdown-item" href="#">Domain Backorder</a></li>
+                            <li><a class="dropdown-item" href="#">Domain Privacy Protection</a></li>
+                            <li><a class="dropdown-item" href="#">Sewa Domain</a></li>
+                            <li><a class="dropdown-item" href="#">Jual Beli & Lelang Domain</a></li>
+                            <li><a class="dropdown-item" href="#">Domain Negotiator/Broker</a></li>
+                            <li><a class="dropdown-item" href="#">Private Label Domain Reseller</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold">
+                            Email Suite
+                            <i class="fa-solid fa-angle-down" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Titan Email</a></li>
+                            <li><a class="dropdown-item" href="#">EC Collaboration Suite</a></li>
+                            <li><a class="dropdown-item" href="#">Microsoft 365</a></li>
+                            <li><a class="dropdown-item" href="#">Google Workspace</a></li>
+                            <li><a class="dropdown-item" href="#">Mailtogo</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold">
+                            Services
+                            <i class="fa-solid fa-angle-down" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">SSL</a></li>
+                            <li><a class="dropdown-item" href="#">Jual/Beli Backlink</a></li>
+                            <li><a class="dropdown-item" href="#">Extended Support</a></li>
+                            <li><a class="dropdown-item" href="#">Manage The Box Server</a></li>
+                            <li><a class="dropdown-item" href="#">Offline Backup</a></li>
+                            <li><a class="dropdown-item" href="#">Advanced Backup</a></li>
+                            <li><a class="dropdown-item" href="#">Hosting Insurance</a></li>
+                            <li><a class="dropdown-item" href="#">Licenses</a></li>
+                            <li><a class="dropdown-item" href="#">Diego VPN</a></li>
+                            <li><a class="dropdown-item" href="#">Digital Signature</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold">
+                            Internet Access
+                            <i class="fa-solid fa-angle-down" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Broadband Home</a></li>
+                            <li><a class="dropdown-item" href="#">Broadband Business</a></li>
+                            <li><a class="dropdown-item" href="#">Broadband Wireless</a></li>
+                            <li><a class="dropdown-item" href="#">Check Coverage</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold">
+                            Customer Care
+                            <i class="fa-solid fa-angle-down" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Pembayaran</a></li>
+                            <li><a class="dropdown-item" href="#">Bantuan 24 Jam</a></li>
+                            <li><a class="dropdown-item" href="#">Knowledge Base</a></li>
+                            <li><a class="dropdown-item" href="#">FAQ</a></li>
+                            <li><a class="dropdown-item" href="#">Blog</a></li>
+                            <li><a class="dropdown-item" href="#">Kontak</a></li>
+                        </ul>
+                    </div>
+        
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="font-weight: bold">Promo</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="nav-brand-custom w-100px ms-lg-auto text-end text-xl-center end-0">
+                <button type="button" class="btn-outline">Log in</button>
+            </div>
+        </div>
+    </nav>
+
+    <div class="position-relative">
+        <div class="position-absolute start-0">
+            <h1 class="judul-atas">Cloud Hosting</h1> 
+                <h1>
+                    <span class="judul-tengah">untuk</span>
+                    <span class="judul-tengah-1">Kesuksesan</span>
+                </h1>
+                <h1 class="judul-bawah">Websitemu!</h1>
+        </div>
+
+        <div class="position-absolute start-50 end-0 top-50">
+            <p class="paragraf">Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman, dan Dukungan 24 jam untuk memenuhi kebutuhan hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3.</p>
+            <p class="hastag">#QwordsYourSuccess</p>
+        </div>
+        </div>
+        
+    </div>
+    
+    
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Qwords\resources\views/home.blade.php ENDPATH**/ ?>
